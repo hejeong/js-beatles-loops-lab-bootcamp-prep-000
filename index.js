fd98d7e5809a1bed69788d9535713e278d4123dd
Jonathan Hong <jonhong777@gmail.com>
2018-06-14 19:02:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+
+  for(var i = 0; i < musicians.length; i++){
+    array[i] = musicians[i] + " plays " + instruments[i]
+  }
+
+  return array
+}
+
+function johnLennonFacts(facts){
+  var count = facts.length - 1
+  while(count >= 0){
+    facts[length] += "!!!"
+  }
+  return facts
+}
